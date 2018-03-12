fcff5eaa781406d4ca04831c53e0a493a017c014
Kai Blin <kblin@biosustain.dtu.dk>
2018-03-12 11:50:27 +0100

    messages: Split out the email templates for easier reuse
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- dispatcher/mail.py
+++ dispatcher/mail.py
@@ -3,6 +3,12 @@ import aiosmtplib
 import asyncio
 from email.mime.text import MIMEText
 
+from dispatcher.messages import (
+    message_template,
+    success_template,
+    failure_template,
+    error_message_template
+)
 
 class EmailConfig:
     """Class collection all email-related configuration"""
@@ -156,42 +162,3 @@ async def _send_mail(app, message):
             tries += 1
             await asyncio.sleep(30)
 
-message_template = """Dear {c.tool} user,
-
-The {c.tool} job {j.job_id} you submitted on {j.added} with the filename
-'{j.filename}' has finished with status {j.state}.
-
-{action_string}
-
-If you found {c.tool} useful, please check out
-{c.base_url}/#!/about
-for information on how to cite {c.tool}.
-"""
-
-success_template = """You can find the results on
-{c.base_url}/upload/{j.job_id}/index.html
-
-Results will be kept for one month and then deleted automatically.
-"""
-
-failure_template = """It produced the following error messages:
-{errors}
-
-Please contact {c.support} to resolve the issue."""
-
-error_message_template = """The {c.tool} job {j.job_id} has failed.
-Dispatcher: {j.dispatcher}
-Input file: {c.base_url}/upload/{j.job_id}/{j.filename}
-GFF file: {c.base_url}/upload/{j.job_id}/{j.gff3}
-Log file: {c.base_url}/upload/{j.job_id}/{j.job_id}.log
-User email: {j.email}
-State: {j.state}
-Errors:
-{errors}
-
-Warnings:
-{warnings}
-
-Backtrace:
-{backtrace}
-"""
