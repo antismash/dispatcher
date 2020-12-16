e4baf8939449228745cb1ab40cf9c436b7c2236b
Kai Blin <kblin@biosustain.dtu.dk>
2020-12-16 14:45:07 +0100

    chore: Bump antismash-models minimum version
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
     'aiosmtplib',
     'envparse',
     'aiofiles',
-    'antismash_models >= 0.1.20',
+    'antismash_models >= 0.1.21',
     'aio-standalone',
     'toml',
 ]
